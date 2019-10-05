2bd34cece5ec7a3f6893f390e47b4e339b1cff99
Morph1sm <shadownansen@gmail.com>
2019-10-05 12:28:38 -0500

    Added region model with a Morph collection. Added card layout for both morphs and regions.

---
+++ Morphology/Region.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace Morphology
+{
+    public class Region : INotifyPropertyChanged
+    {
+        private string _name;
+        // Signifies VaM's built-in region names
+        private readonly bool _standard;
+
+        public Morphs morphs = new Morphs();
+
+        public Region(string name)
+        {
+            _name = name;
+            _standard = false;
+        }
+        public Region(string name, bool standard)
+        {
+            _name = name;
+            _standard = standard;
+        }
+        public bool IsStandard
+        {
+            get { return _standard; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_standard)
+                {
+
+                }
+
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Info
+        {
+            get
+            {
+                long count = morphs.Count;
+
+                if (count == 0) {
+                    return "empty";
+                }
+
+                if (count == 1)
+                {
+                    return "1 morph";
+                }
+
+                return count + " morphs";
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public override string ToString() => _name;
+        protected void OnPropertyChanged(string info)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
---
+++ Morphology/Regions.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Morphology
+{
+    public class Regions : ObservableCollection<Region>
+    {
+        public Regions()
+        {
+            // VaM Standard Regions
+            Add(new Region("Morph/Anus", true));
+            Add(new Region("Morph/Arms", true));
+            Add(new Region("Morph/Back", true));
+            Add(new Region("Morph/Body", true));
+            Add(new Region("Morph/Chest/Areola", true));
+            Add(new Region("Morph/Chest/Breasts", true));
+            Add(new Region("Morph/Chest/Nipples", true));
+            Add(new Region("Morph/Feet", true));
+            Add(new Region("Morph/Genitalia", true));
+            Add(new Region("Morph/Hands", true));
+            Add(new Region("Morph/Head", true));
+            Add(new Region("Morph/Head/Brow", true));
+            Add(new Region("Morph/Head/Cheeks", true));
+            Add(new Region("Morph/Head/Chin", true));
+            Add(new Region("Morph/Head/Ears", true));
+            Add(new Region("Morph/Head/Eyes", true));
+            Add(new Region("Morph/Head/Face", true));
+            Add(new Region("Morph/Head/Jaw", true));
+            Add(new Region("Morph/Head/Mouth", true));
+            Add(new Region("Morph/Head/Mouth/Teeth", true));
+            Add(new Region("Morph/Head/Mouth/Tongue", true));
+            Add(new Region("Morph/Head/Nose", true));
+            Add(new Region("Morph/Head/Shape", true));
+            Add(new Region("Morph/Hip", true));
+            Add(new Region("Morph/Legs", true));
+            Add(new Region("Morph/Neck", true));
+            Add(new Region("Morph/UpperBody", true));
+            Add(new Region("Morph/Waist", true));
+            Add(new Region("Pose/Arms", true));
+            Add(new Region("Pose/Chest", true));
+            Add(new Region("Pose/Feet/Left/Toes", true));
+            Add(new Region("Pose/Feet/Right/Toes", true));
+            Add(new Region("Pose/Hands/Left", true));
+            Add(new Region("Pose/Hands/Left/Fingers", true));
+            Add(new Region("Pose/Hands/Right", true));
+            Add(new Region("Pose/Hands/Right/Fingers", true));
+            Add(new Region("Pose/Head/Brow", true));
+            Add(new Region("Pose/Head/Cheeks", true));
+            Add(new Region("Pose/Head/Expressions", true));
+            Add(new Region("Pose/Head/Eyes", true));
+            Add(new Region("Pose/Head/Jaw", true));
+            Add(new Region("Pose/Head/Mouth", true));
+            Add(new Region("Pose/Head/Mouth/Lips", true));
+            Add(new Region("Pose/Head/Mouth/Tongue", true));
+            Add(new Region("Pose/Head/Nose", true));
+            Add(new Region("Pose/Head/Visemes", true));
+
+
+            // TODO: Remove these test custom regions
+            Add(new Region("__Alter3go__"));
+
+            Region bbw = new Region("Big Girl Morphs");
+            bbw.morphs.Add(new Morph("PHMBGMChin01", "Big Girl Morphs", "Chin 01"));
+            bbw.morphs.Add(new Morph("PHMBGMChin02", "Big Girl Morphs", "Chin 02"));
+            Add(bbw);
+
+            Region af = new Region("Asian Faces");
+            bbw.morphs.Add(new Morph("AFNose01", "Asian Faces", "Nose 01"));
+            bbw.morphs.Add(new Morph("AFNose02", "Asian Faces", "Nose 02"));
+            Add(af);
+
+        }
+    }
+}
