21e7a35a0b6e5fd5801bfd616e2c10249d9d0057
Morph1sm <shadownansen@gmail.com>
2019-10-12 16:12:59 -0500

    Added some app meta data

--- Morphology/Properties/AssemblyInfo.cs
+++ Morphology/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Morphology")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Virt-a-Mate morph library organizer.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Morph1sm")]
 [assembly: AssemblyProduct("Morphology")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("MIT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
